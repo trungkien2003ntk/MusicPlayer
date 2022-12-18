9f4e7a24b61de81a29de70b85f3413afafc656d3
trungkien2003ntk <trungkien2003ntk@gmail.com>
2022-12-19 00:44:33 +0700

    Add SongControl VVM

---
+++ MVVM_Basics/ViewModels/SongControlViewModel.cs
@@ -0,0 +1,130 @@
+﻿using Id3;
+using MVVM_Basics.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace MVVM_Basics.ViewModels
+{
+    public class SongControlViewModel : BaseViewModel, ISoundPlayer
+    {
+        #region Static 
+        public static Queue<Song> songQueue = new Queue<Song>();
+        public static Song CurrentPlayingSong = new Song();
+        public static bool HasSongLoaded = false;
+        #endregion Static 
+
+
+        #region Fields
+        private string _MusicPath = "C:\\Users\\ADMIN\\Downloads\\Music\\Songs\\Tăng Duy Tân - Bên Trên Tầng Lầu - Official Lyric Video.mp3";
+        private string _Title;
+        private string _Artist;
+        private BitmapImage _Thumbnail;
+
+        private Uri _Source;
+        private bool _IsPlaying;
+        #endregion Fields
+
+
+        #region Properties
+        public string MusicPath { get => _MusicPath; set => _MusicPath = value; }
+        public string Title { get => _Title; set { _Title = value; OnPropertyChanged(); } }
+        public string Artist { get => _Artist; set { _Artist = value; OnPropertyChanged(); } }
+        public BitmapImage Thumbnail { get => _Thumbnail; set { _Thumbnail = value; OnPropertyChanged(); } }
+
+        public Uri Source { get => _Source; set { _Source = value; OnPropertyChanged(); } }
+        public bool IsPlaying
+        {
+            get => _IsPlaying;
+            set
+            {
+                _IsPlaying = value;
+                if (HasSongLoaded)
+                    if (_IsPlaying)
+                        Play();
+                    else
+                        Pause();
+            }
+        }
+        #endregion Properties
+
+
+        #region Event
+        public event Action Play;
+        public event Action Pause;
+        #endregion Event
+
+
+        #region Constructor
+        public SongControlViewModel()
+        {
+            // Set for testing UI
+            CurrentPlayingSong.PcLink = MusicPath;
+            HasSongLoaded = true;
+
+            using (var mp3 = new Mp3(MusicPath))
+            {
+                Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
+
+                byte[] bytes = Encoding.Default.GetBytes("Kiến");
+                Title = tag.Title.ToString();
+                Artist = tag.Artists.ToString();
+
+                Thumbnail = LoadImage(tag.Pictures[0].PictureData);
+            }
+
+            _IsPlaying = false;
+
+            if (HasSongLoaded)
+            {
+                _Source = new Uri(CurrentPlayingSong.PcLink);
+            }
+        }
+        #endregion Constructor
+
+
+        private static BitmapImage LoadImage(byte[] imageData)
+        {
+            if (imageData == null || imageData.Length == 0) return null;
+            var image = new BitmapImage();
+            using (var mem = new MemoryStream(imageData))
+            {
+                mem.Position = 0;
+                image.BeginInit();
+                image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.UriSource = null;
+                image.StreamSource = mem;
+                image.EndInit();
+            }
+            image.Freeze();
+            return image;
+        }
+
+        public static void ReadID3()
+        {
+            string[] musicFiles = Directory.GetFiles(@"C:\Users\ADMIN\Downloads\Music\Songs", "*.mp3");
+
+            foreach (string musicFile in musicFiles)
+            {
+                using (var mp3 = new Mp3(musicFile))
+                {
+                    Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
+
+                    byte[] bytes = Encoding.Default.GetBytes("Kiến");
+                    string title = Encoding.UTF32.GetString(bytes);
+
+                    Console.WriteLine("Title: {0}", title);
+                    Console.WriteLine("Artist: {0}", tag.Artists.ToString());
+                    Console.WriteLine("Album: {0}", tag.Album.ToString());
+                    Console.WriteLine("Album: {0}", tag.Pictures[0].MimeType);
+                    Console.ReadKey(true);
+                }
+            }
+        }
+    }
+}
---
+++ MVVM_Basics/Views/SongControl.xaml.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using Microsoft.WindowsAPICodePack.Shell;
+using MVVM_Basics.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace MVVM_Basics.Views
+{
+    /// <summary>
+    /// Interaction logic for SongControl.xaml
+    /// </summary>
+    public partial class SongControl : UserControl
+    {
+        ISoundPlayer _SoundPlayer;
+        double savedSliderVolumeValue;
+
+        public SongControl()
+        {
+            InitializeComponent();
+
+            DataContextChanged += OnDataContextChanged;
+            Unloaded += OnUnloaded;
+
+            TxblPlayedDuration.Text = mediaPlayer.Position.ToString(@"mm\:ss");
+            TxblTotalDuration.Text = GetMediaDuration(mediaPlayer.Source.ToString()).ToString(@"mm\:ss");
+            sliderTime.Maximum = GetMediaDuration(mediaPlayer.Source.ToString()).TotalSeconds;
+        }
+        
+
+        void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs args)
+        {
+            if (DataContext is ISoundPlayer player && _SoundPlayer != player)
+            {
+                if (_SoundPlayer != null)
+                {
+                    _SoundPlayer.Play -= OnPlay;
+                    _SoundPlayer.Pause -= OnPause;
+                }
+
+                _SoundPlayer = player;
+                _SoundPlayer.Play += OnPlay;
+                _SoundPlayer.Pause += OnPause;
+                //_SoundPlayer.OnSongChanged += _SoundPlayer_OnSongChanged;
+            }
+        }
+
+        private void _SoundPlayer_OnSongChanged(string songPath)
+        {
+            if (!string.IsNullOrEmpty(songPath))
+            {
+                mediaPlayer.Source = null;
+                mediaPlayer.Source = new Uri(songPath);
+            }
+        }
+
+        void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            if (_SoundPlayer != null)
+                _SoundPlayer.Play -= OnPlay;
+        }
+
+        private void OnPlay() => mediaPlayer.Play();
+        private void OnPause() => mediaPlayer.Pause();
+
+        private void sliderTime_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TimeSpan timeSpan = TimeSpan.FromSeconds((int)sliderTime.Value);
+            TimeSpan mediaTimeSpan = new TimeSpan(0, 0, 0, 0, (int)timeSpan.TotalMilliseconds);
+
+            TxblPlayedDuration.Text = timeSpan.ToString(@"mm\:ss");
+            mediaPlayer.Position = mediaTimeSpan;
+        }
+
+        private TimeSpan GetMediaDuration(string filePath)
+        {
+            using (var shell = ShellObject.FromParsingName(filePath))
+            {
+                IShellProperty prop = shell.Properties.System.Media.Duration;
+                var t = (ulong)prop.ValueAsObject;
+                return TimeSpan.FromTicks((long)t);
+            }
+        }
+
+        private void sliderVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Change Volume Button Icon
+            PackIcon icon = new PackIcon()
+            {
+                Width = 25,
+                Height = 25,
+            };
+
+            if (sliderVolume.Value > 60)
+            {
+                icon.Kind = PackIconKind.VolumeHigh;
+            }
+            else if (20 < sliderVolume.Value && sliderVolume.Value <= 60) 
+            {
+                icon.Kind = PackIconKind.VolumeMedium;
+            }
+            else if (0 < sliderVolume.Value && sliderVolume.Value <= 20) 
+            {
+                icon.Kind = PackIconKind.VolumeLow;
+            }
+            else
+            {
+                icon.Kind = PackIconKind.VolumeMute;
+            }
+
+            btnVolume.Content = icon;
+
+            // Change MediaElment's Volume
+            mediaPlayer.Volume = sliderVolume.Value / 100f;
+        }
+
+        private void btnVolumeMax_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sliderVolume.Value != 0)
+            {
+                savedSliderVolumeValue = sliderVolume.Value;
+                sliderVolume.Value = 0;
+            }
+            else
+            {
+                sliderVolume.Value = savedSliderVolumeValue;
+            }
+        }
+    }
+}
