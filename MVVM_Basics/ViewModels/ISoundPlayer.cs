e766e4e4d1ead20a23a5216c2752d959a8b312d0
trungkien2003ntk <trungkien2003ntk@gmail.com>
2022-12-19 00:43:48 +0700

    Create ISoundPlayer.cs

---
+++ MVVM_Basics/ViewModels/ISoundPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Basics.ViewModels
+{
+    public interface ISoundPlayer
+    {
+        event Action Play;
+        event Action Pause;
+
+        public delegate void ChangeSong(string songPath);
+        //public event ChangeSong? OnSongChanged;
+    }
+}
