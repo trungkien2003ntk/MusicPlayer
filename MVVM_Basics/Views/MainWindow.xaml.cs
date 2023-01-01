597377fe759da6e537b4589d0c409f04d414a6ad
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-01-01 14:29:16 +0700

    Update MainWindow.xaml.cs

--- MVVM_Basics/Views/MainWindow.xaml.cs
+++ MVVM_Basics/Views/MainWindow.xaml.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MVVM_Basics.Models;
+using MVVM_Basics.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace MVVM_Basics.Views
+namespace MVVM_Basics.Views;
+
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
