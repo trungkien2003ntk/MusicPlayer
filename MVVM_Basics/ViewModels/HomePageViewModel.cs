0d20f67aad435c43e3d35593c57ea392279bf652
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-01-01 14:27:08 +0700

    Create HomePageViewModel.cs

---
+++ MVVM_Basics/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Basics.ViewModels
+{
+    public class HomePageViewModel : ViewModelBase
+    {
+        public HomePageViewModel()
+        {
+        }
+    }
+}
