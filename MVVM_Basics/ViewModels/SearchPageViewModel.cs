01fd0271b1b1072c0917563bd906e8e57f8444be
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-01-01 14:28:01 +0700

    Create SearchPageViewModel.cs

---
+++ MVVM_Basics/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Basics.ViewModels
+{
+
+    public class SearchPageViewModel : ViewModelBase
+    {
+        public SearchPageViewModel()
+        {
+        }
+    }
+}
