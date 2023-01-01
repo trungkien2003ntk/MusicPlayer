51593db73afd8a4d2888a613d0460f5cd505cfc7
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-01-01 12:13:50 +0700

    Debug Files

---
+++ MVVM_Basics/EventAndCommandHandlers/EventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MVVM_Basics.EventAndCommandHandlers;
+
+public class EventArgs<T> : EventArgs
+{
+    public EventArgs(T value)
+    {
+        Value = value;
+    }
+    public T Value { get; private set; }
+}
---
+++ MVVM_Basics/EventAndCommandHandlers/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM_Basics.EventAndCommandHandlers
+{
+    class RelayCommand<T> : ICommand
+    {
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
+
+        public RelayCommand(Predicate<T> canExecute, Action<T> execute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            try
+            {
+                return _canExecute == null || _canExecute((T)parameter!);
+            }
+            catch
+            {
+                return true;
+            }
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute((T)parameter!);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; CanExecuteChangedInternal += value; }
+            remove { CommandManager.RequerySuggested -= value; CanExecuteChangedInternal -= value; }
+        }
+
+        private event EventHandler? CanExecuteChangedInternal;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChangedInternal?.Raise(this);
+        }
+    }
+
+}
