9ff4472bf56bbf4f349d4600a9057c16c3623eb9
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-01-01 14:27:52 +0700

    Update PlaylistViewModel for showing playlistView

--- MVVM_Basics/ViewModels/PlaylistViewModel.cs
+++ MVVM_Basics/ViewModels/PlaylistViewModel.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Id3;
+using MVVM_Basics.Models;
+using System;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace MVVM_Basics.ViewModels
 {
-    public class PlaylistViewModel : BaseViewModel
+    public class PlaylistViewModel : ViewModelBase
     {
+        private Playlist _CurrentPlaylist;
+        public Playlist CurrentPlaylist { get => _CurrentPlaylist; set { _CurrentPlaylist= value; OnPropertyChanged(); } }
+
+        private int _TotalSongs;
+        public int TotalSongs { get => _TotalSongs; set { _TotalSongs = value; OnPropertyChanged(); } }
+
+        private string _TotalDuration;
+        public string TotalDuration { get => _TotalDuration; set { _TotalDuration = value; OnPropertyChanged(); } }
+
+        public PlaylistViewModel()
+        {
+
+        }
+        public PlaylistViewModel(Playlist playlist)
+        {
+            CurrentPlaylist = playlist;
+            TotalSongs = CurrentPlaylist.PlaylistSongs.Count;
+            
+            _TotalDuration = GetTotalDuration(CurrentPlaylist).ToString("%h' hr '%m' min'");
+        }
+
+        private TimeSpan GetTotalDuration(Playlist currentPlaylist)
+        {
+            TimeSpan result = new TimeSpan(0, 0, 0);
+
+            foreach (PlaylistSong playlistSong in currentPlaylist.PlaylistSongs)
+            {
+                using (var database = new MusicPlayerVpContext())
+                {
+                    var MusicPath = database.Songs.Where(s => s.Id == playlistSong.SongId).First().PcLink;
+                    
+                    using (var mp3 = new Mp3(MusicPath))
+                    {
+                        Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X);
+
+                        result += tag.Length.Value;
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
