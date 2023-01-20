eec9dacd48048fa5f7a2d326540ffa1cdb25ffdc
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-01-21 00:18:09 +0700

    Update Resource Files

---
+++ MVVM_Basics/ResourcesXAML/PlaylistResource.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MVVM_Basics.ResourcesXAML
+{
+    public partial class PlaylistResource : System.Windows.ResourceDictionary
+    {
+        public PlaylistResource()
+        {
+            
+        }
+
+        private void ContextMenu_Closed(object sender, RoutedEventArgs e)
+        {
+            var thisContextMenu = sender as ContextMenu;
+            ListView listviewParent = thisContextMenu.PlacementTarget as ListView;
+
+            listviewParent.SelectedIndex = -1;
+        }
+    }
+}
