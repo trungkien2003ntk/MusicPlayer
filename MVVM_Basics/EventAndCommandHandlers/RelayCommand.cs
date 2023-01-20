e23d6214b821377e4dddd96eb559ff47a7fad7a1
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-01-21 00:16:26 +0700

    Debug Files

--- MVVM_Basics/EventAndCommandHandlers/RelayCommand.cs
+++ MVVM_Basics/EventAndCommandHandlers/RelayCommand.cs
@@ -1,54 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace MVVM_Basics.EventAndCommandHandlers
+namespace MVVM_Basics.EventAndCommandHandlers;
+
+class RelayCommand<T> : ICommand
 {
-    class RelayCommand<T> : ICommand
+    private readonly Predicate<T> _canExecute;
+    private readonly Action<T> _execute;
+
+    public RelayCommand(Predicate<T> canExecute, Action<T> execute)
     {
-        private readonly Predicate<T> _canExecute;
-        private readonly Action<T> _execute;
+        if (execute == null)
+            throw new ArgumentNullException("execute");
+        _canExecute = canExecute;
+        _execute = execute;
+    }
 
-        public RelayCommand(Predicate<T> canExecute, Action<T> execute)
+    public bool CanExecute(object? parameter)
+    {
+        try
         {
-            if (execute == null)
-                throw new ArgumentNullException("execute");
-            _canExecute = canExecute;
-            _execute = execute;
+            return _canExecute == null || _canExecute((T)parameter!);
         }
-
-        public bool CanExecute(object? parameter)
+        catch
         {
-            try
-            {
-                return _canExecute == null || _canExecute((T)parameter!);
-            }
-            catch
-            {
-                return true;
-            }
-        }
-
-        public void Execute(object? parameter)
-        {
-            _execute((T)parameter!);
-        }
-
-        public event EventHandler? CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; CanExecuteChangedInternal += value; }
-            remove { CommandManager.RequerySuggested -= value; CanExecuteChangedInternal -= value; }
-        }
-
-        private event EventHandler? CanExecuteChangedInternal;
-
-        public void RaiseCanExecuteChanged()
-        {
-            CanExecuteChangedInternal?.Raise(this);
+            return true;
         }
     }
 
+    public void Execute(object? parameter)
+    {
+        _execute((T)parameter!);
+    }
+
+    public event EventHandler? CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; CanExecuteChangedInternal += value; }
+        remove { CommandManager.RequerySuggested -= value; CanExecuteChangedInternal -= value; }
+    }
+
+    private event EventHandler? CanExecuteChangedInternal;
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChangedInternal?.Raise(this);
+    }
 }
