dddcc57ccbdd7f61a649cbed43c4f294224eb5d6
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-02-09 19:07:26 +0700

    Customize view for notifications

---
+++ MVVM_Basics/Views/MessageAndNotification/CustomMessageBox.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace MVVM_Basics.Views.MessageAndNotification;
+
+public enum MessageBoxType
+{
+    // First MessageBox
+    AlreadyAddedToPlaylist
+}
+
+public partial class CustomMessageBox : Window, INotifyPropertyChanged
+{
+    private string _PlaylistName;
+
+
+    private string _MessageBoxMessage;
+    public string MessageBoxMessage
+    {
+        get { return _MessageBoxMessage; }
+        set { _MessageBoxMessage = value; OnPropertyChanged(); }
+    }
+
+
+    private string _MessageBoxTitle;
+    public string MessageBoxTitle
+    {
+        get { return _MessageBoxTitle; }
+        set { _MessageBoxTitle = value; OnPropertyChanged(); }
+    }
+
+
+    private string _YesBtnContent;
+    public string YesBtnContent
+    {
+        get { return _YesBtnContent; }
+        set { _YesBtnContent = value; OnPropertyChanged(); }
+    }
+
+
+    private string _NoBtnContent;
+    public string NoBtnContent
+    {
+        get { return _NoBtnContent; }
+        set { _NoBtnContent = value; OnPropertyChanged(); }
+    }
+
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public CustomMessageBox(MessageBoxType messageBoxType)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+        InitializeComponent();
+
+        // Set the owner for the startup location
+        Owner = Application.Current.MainWindow;
+
+        // Set DataContext for binding to the string in this class
+        DataContext = this;
+
+        InitializeText(messageBoxType);
+    }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    public CustomMessageBox(string playlistName, MessageBoxType messageBoxType = MessageBoxType.AlreadyAddedToPlaylist)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+        InitializeComponent();
+
+        // Set the owner for the startup location
+        Owner = Application.Current.MainWindow;
+
+        // Set DataContext for binding to the string in this class
+        DataContext = this;
+
+        _PlaylistName = playlistName;
+
+        InitializeText(messageBoxType);
+    }
+
+    protected override void OnDeactivated(EventArgs e)
+    {
+        base.OnDeactivated(e);
+
+        if (DialogResult == null)
+        {
+            Close();
+        }
+    }
+
+    private void InitializeText(MessageBoxType messageBoxType)
+    {
+        if (messageBoxType == MessageBoxType.AlreadyAddedToPlaylist)
+        {
+            MessageBoxMessage = $"This is already in your \"{_PlaylistName}\" playlist";
+            MessageBoxTitle = "Already added";
+            YesBtnContent = "Add anyway";
+            NoBtnContent = "Don't add";
+        }
+    }
+
+    private void btnYes_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+    }
+
+    private void btnNo_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+    }
+
+
+    // Implement InotifyPropertyChanged
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ MVVM_Basics/Views/MessageAndNotification/CustomizeToast.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Channels;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+
+namespace MVVM_Basics.Views.MessageAndNotification;
+
+public enum NotificationType
+{
+    Added,
+    Removed,
+    AddedPlaylist,
+    RemovedPlaylist,
+}
+
+public enum TargetType
+{
+    LikedSongs,
+    Artists,
+    Library,
+    Playlist,
+}
+
+public partial class CustomizeToast : UserControl, INotifyPropertyChanged
+{
+    private readonly Dictionary<NotificationType, string> NotificationStrings = new()
+    {
+        {NotificationType.Added, "Added to your " },
+        {NotificationType.Removed, "Removed from your " },
+        {NotificationType.AddedPlaylist, "Added to " },
+        {NotificationType.RemovedPlaylist, "Removed from " },
+    };
+    private readonly Dictionary<TargetType, string> TargetStrings = new()
+    {
+        { TargetType.Artists, "Artists" },
+        { TargetType.LikedSongs, "Liked Songs" },
+        { TargetType.Library, "Library" },
+        { TargetType.Playlist, "Playlist" },
+    };
+
+
+    
+    private const double TIMEOUT_THRESHOLD = 1.5;
+    private const double FADE_OUT_DURATION = 0.5;
+    
+    private DispatcherTimer _Timer;
+    private DoubleAnimation _FadeOutAnimation;
+    private double _CountSec = 0;
+
+
+    private string _Notification = null!;
+    public string Notification
+    {
+		get { return _Notification; }
+		set { _Notification = value; OnPropertyChanged(); }
+	}
+
+    private string _Target = null!;
+    public string Target
+    {
+        get { return _Target; }
+        set { _Target = value; OnPropertyChanged(); }
+    }
+
+
+    public CustomizeToast(NotificationType notificationType, TargetType targetType)
+    {
+        InitializeComponent();
+
+        // Initialize Timer
+        _Timer = new DispatcherTimer()
+        {
+            Interval = TimeSpan.FromSeconds(0.5),
+            IsEnabled = false,
+        };
+        _Timer.Tick += OnTimeTick;
+
+
+        // Initialize Animation
+        _FadeOutAnimation = new()
+        {
+            From = 1,
+            To = 0,
+            Duration = new Duration(TimeSpan.FromSeconds(FADE_OUT_DURATION)),
+        };
+
+
+        // Set DataContext for binding
+        DataContext = this;
+
+
+        // Set Notification and Destination
+        Notification = NotificationStrings[notificationType];
+        Target = TargetStrings[targetType];
+    }
+
+    private void UserControl_Loaded(object sender, System.Windows.RoutedEventArgs e)
+    {
+        _Timer.Start();
+    }
+
+    private void OnTimeTick(object? sender, EventArgs e)
+    {
+        _CountSec += 0.5;
+
+        if (_CountSec == TIMEOUT_THRESHOLD)
+        {
+            BeginAnimation(OpacityProperty, _FadeOutAnimation);
+        }
+
+        if (_CountSec == TIMEOUT_THRESHOLD + FADE_OUT_DURATION)
+        {
+            var parentAsContentControl = (ContentControl)Parent;
+            
+            if (parentAsContentControl != null)
+            {
+                parentAsContentControl.Content = null;
+            }
+
+            _CountSec = 0;
+            _Timer.Stop();
+        }
+
+    }
+
+    // Implement InotifyPropertyChanged
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
