c41372d3313edc3247a563a6a98ab33173ee1277
trungkien2003ntk <trungkien2003ntk@gmail.com>
2023-03-06 23:25:13 +0700

    EnableRetryOnFailure

--- MVVM_Basics/Models/MusicPlayerVpContext.cs
+++ MVVM_Basics/Models/MusicPlayerVpContext.cs
@@ -37,7 +37,8 @@ namespace MVVM_Basics.Models
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 //optionsBuilder.UseSqlServer("Data Source=.\\sqlexpress;Initial Catalog=MUSIC_PLAYER_VP;Integrated Security=True;TrustServerCertificate=True");
-                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["MusicPlayerDatabase"].ConnectionString);
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["MusicPlayerDatabase"].ConnectionString,
+                    option => option.EnableRetryOnFailure());
             }
         }
 
